c7cd0710fb1b70eb8eb320e18c6117ec0e55d43d
Chapien <j.robsahm@digipen.edu>
2014-08-08 02:00:07 -0700

    Fixed int to float conversion
    See title

--- Roguelike/Engine/StandardShapes.h
+++ Roguelike/Engine/StandardShapes.h
@@ -5,9 +5,9 @@
 
 namespace Shapes
 {
-  Model *makeRectangle(ID3D11Device *device, math::Vector2D size, math::Vector2D center = {0, 0});
-  Model *makeCircle(ID3D11Device *device, unsigned sides, float radius, math::Vector color = {1,1,1,1});
-  Model *makeCube(ID3D11Device *device, float size, math::Vector color = {1,1,1,1});
+  Model *makeRectangle(ID3D11Device *device, math::Vector2D size, math::Vector2D center = {0.f, 0.f});
+  Model *makeCircle(ID3D11Device *device, unsigned sides, float radius, math::Vector color = {1.f,1.f,1.f,1.f});
+  Model *makeCube(ID3D11Device *device, float size, math::Vector color = {1.f,1.f,1.f,1.f});
 }
 
 

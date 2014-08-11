6ae9bf567b0bbb4a68678f31af3550b7ca377940
Chapien <j.robsahm@digipen.edu>
2014-08-11 03:19:07 -0700

    Implemented can handle

--- Roguelike/Engine/Entity.cpp
+++ Roguelike/Engine/Entity.cpp
@@ -1,10 +1,19 @@
 /*********************************
- * Entity.h
+ * Entity.cpp
  * Jake Robsahm
  * Created 2014/08/11
  *********************************/
 
 #include "Common.h"
 
+bool Entity::canHandle(const Events::EventMessage& e)
+{
+  auto iterator = _events.find(e.eventId);
+  return iterator != _events.end;
+}
 
+void handle(Events::EventMessage& e)
+{
+
+}
 

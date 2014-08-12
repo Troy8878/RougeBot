1659c0add3624e96942871a91fd2a31afcb74d66
Chapien <j.robsahm@digipen.edu>
2014-08-12 13:44:27 -0700

    Revert "Implemented can handle"
    This reverts commit 6ae9bf567b0bbb4a68678f31af3550b7ca377940.

--- Roguelike/Engine/Entity.cpp
+++ Roguelike/Engine/Entity.cpp
@@ -1,19 +1,10 @@
 /*********************************
- * Entity.cpp
+ * Entity.h
  * Jake Robsahm
  * Created 2014/08/11
  *********************************/
 
 #include "Common.h"
 
-bool Entity::canHandle(const Events::EventMessage& e)
-{
-  auto iterator = _events.find(e.eventId);
-  return iterator != _events.end;
-}
 
-void handle(Events::EventMessage& e)
-{
-
-}
 

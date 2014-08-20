6ec913f9e85d096519586274299b79654cd94508
Connor Hilarides <connorcpu@live.com>
2014-08-20 15:11:31 -0700

    modified the build script a little

--- build.ps1
+++ build.ps1
@@ -1,3 +1,3 @@
 Invoke-BatchFile "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\vcvars32.bat"
 
-msbuild Roguelike\Roguelike.sln /v:m
+msbuild Roguelike\Roguelike.sln /v:m /property:Configuration=$env:Configuration

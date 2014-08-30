f69061d7baeb1819a157007f4f1fd4240ace5633
Connor Hilarides <connorcpu@live.com>
2014-08-29 23:55:44 -0700

    You know, somebody might like to have math :P

--- Roguelike/Engine/RubyInit.cpp
+++ Roguelike/Engine/RubyInit.cpp
@@ -51,17 +51,26 @@ extern "C"
   void mrb_mruby_fiber_gem_init(mrb_state* mrb);
   void mrb_mruby_gamestuff_gem_init(mrb_state *mrb);
   void mrb_mruby_print_gem_init(mrb_state* mrb);
+  void mrb_mruby_math_gem_init(mrb_state* mrb);
 
   void mrb_init_mrbgems(mrb_state *mrb)
   {
     ruby::ruby_engine engine{mrb};
 
+    // Fiber gem
     mrb_mruby_fiber_gem_init(mrb);
+
+    // Load print gem (puts is nice to have for debugging :P)
     mrb_mruby_print_gem_init(mrb);
     engine.evaluate_asset("Gems/mrb-print/print.rb");
 
+    // You know, somebody might like to have math :P
+    mrb_mruby_math_gem_init(mrb);
+
+    // Load the core library
     loadRubyRes(mrb, IDR_RB_GEM_ENUMERATOR);
 
+    // Load the gamestuff that I've defined to help out
     mrb_mruby_gamestuff_gem_init(mrb);
   }
 

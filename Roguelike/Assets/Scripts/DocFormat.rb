29453c8d1087be0c5c56e6312bdbc8b553227d5c
Jake Robsahm <j.robsahm@digipen.edu>
2014-09-05 13:48:51 -0700

    Created a documentation template for all Ruby Scripts. Follow it!

---
+++ Roguelike/Assets/Scripts/DocFormat.rb
@@ -0,0 +1,26 @@
+#Documentation Template#
+
+#PLEASE COPY PASTE THIS TEMPLATE ON ALL RUBY FILES#
+
+########################
+# FileName.rb
+# Your Name
+# Created Year/Month/Day
+########################
+
+#Paste this on top of all classes!#
+
+#########################Class Details##########################
+#Enter a summary of the class here, as well as what it inherits
+#and why.
+#######################Property Details#########################
+#List your properties and why they exist here.
+################################################################
+
+#Make sure to document/comment in code as well!#
+#DO THIS OR JAKE WILL MURDER YOU!#
+#Just kidding. Please just do it or you will make me sad panda.#
+
+#Sincerely, 
+#Jake Robsahm
+#The person in charge of the Ruby

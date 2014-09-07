fd46a67d6497e7b0feaca3187e3e2ac2eebdc03a
Chapien <j.robsahm@digipen.edu>
2014-09-06 17:04:08 -0700

    Created equipment hash. Equipping items is now functional.

--- Roguelike/Assets/Scripts/Support/Inventory.rb
+++ Roguelike/Assets/Scripts/Support/Inventory.rb
@@ -11,5 +11,38 @@
 ################################################################
 
 class Inventory
-  #to-do
+  attr_reader  :inventory
+
+  def initialize
+
+    @inventory = []
+    @equipment = {}
+
+  end
+
+  # Function to equip an item/add it to the equipment hash map
+  def equip(slot)
+    # Check to make sure that the slot is valid. If not, raise an exception.
+    if slot != :weapon || slot != :chest || slot != :shield
+      raise "Invalid equipment slot."
+    end
+
+    # Store the item in a variable for now
+    item = @inventory[slot]
+    # Remove the item from the inventory array
+    @inventory[slot] = nil
+
+    # Check if something is already equipped
+    if !@equipment[item.equip_slot].nil?
+      equipped = @equipment[item.equip_slot]
+      @equipment[item.equip_slot] = nil
+      @inventory[slot] = equipped
+    end
+
+    # Insert the item into the equipment at the appropriate slot
+    @equipment[item.equip_slot] = item
+
+  end
+
+
 end

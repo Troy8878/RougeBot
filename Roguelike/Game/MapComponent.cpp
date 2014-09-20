8b35c81e2a3faa3b53444cac1e357073422747dd
Connor Hilarides <connorcpu@live.com>
2014-09-19 18:06:09 -0700

    Implemented Validate.

--- Roguelike/Game/MapComponent.cpp
+++ Roguelike/Game/MapComponent.cpp
@@ -32,6 +32,32 @@ void MapComponent::Initialize(Entity *owner, const std::string& name)
 
 // ----------------------------------------------------------------------------
 
+void MapComponent::DrawingResources::Validate()
+{
+  using namespace D2D1;
+
+  // Get a reference to direct2D
+  auto& d2d = GetGame()->GameDevice->D2D;
+  // Make sure the timestamp is okay
+  if (timestamp >= d2d.ResourceTimestamp)
+    return;
+  // Release resources if it is invalid
+  Release();
+
+  HRESULT hr;
+
+  ID2D1SolidColorBrush *scBrush;
+
+  // Create the line brush
+  hr = d2d.DeviceContext->CreateSolidColorBrush(ColorF(ColorF::White), &scBrush);
+  CHECK_HRESULT(hr);
+  lineBrush = scBrush;
+  // Create the player brush
+  hr = d2d.DeviceContext->CreateSolidColorBrush(ColorF(ColorF::Blue), &scBrush);
+  CHECK_HRESULT(hr);
+  playerBrush = scBrush;
+}
+
 // ----------------------------------------------------------------------------
 
 void MapComponent::DrawingResources::Release()

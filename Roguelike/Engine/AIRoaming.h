885b9b72420e8bcb06d87cfa864b6b8f99c70471
Jake Robsahm <j.robsahm@digipen.edu>
2014-11-05 17:22:04 -0800

    Added AIBehaviour.h

---
+++ Roguelike/Engine/AIRoaming.h
@@ -0,0 +1,13 @@
+/*********************************
+ * AIRoaming.h
+ * Jake Robsahm
+ * Created 2014/11/05
+ *********************************/
+
+#pragma once
+
+#include "AIBehaviour.h"
+
+class AIRoaming : public AIBehaviour
+{
+};
--- Roguelike/Engine/AStarPathfinding.h
+++ Roguelike/Engine/AStarPathfinding.h
@@ -22,6 +22,7 @@ private:
   struct Route
   {
 
+
   };
 
   const WorldSnapshot *world;

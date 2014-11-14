f6d48e648ed16b8ba15a9f3493682126dee6a518
Connor Hilarides <connorcpu@live.com>
2014-11-13 17:28:43 -0800

    ops`

---
+++ Roguelike/Assets/Scripts/Support/ArchHelpers/GameLog.rb
@@ -0,0 +1,17 @@
+#######################
+# GameLog.rb
+# Connor Hilarides
+# Created 2014/09/09
+#######################
+
+module GameLog
+  LOG_DIR = Dir.appdata["DigiPen"]["Roguebot"]["GameLogs"].path
+  LOG_FILE = File.new
+
+  def self.log(data)
+    LOG_FILE.open LOG_DIR + "/game_log.txt"
+    LOG_FILE.write data.inspect
+    LOG_FILE.write ','
+    LOG_FILE.close
+  end
+end

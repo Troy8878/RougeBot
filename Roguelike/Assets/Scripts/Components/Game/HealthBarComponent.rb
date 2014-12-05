1ba61fde3b96667bcda24766f7901ec60f497af4
Mikotey <mikotey@gmail.com>
2014-12-05 03:42:11 -0800

    Health Bars, Random Movement

---
+++ Roguelike/Assets/Scripts/Components/Game/HealthBarComponent.rb
@@ -0,0 +1,29 @@
+#######################
+# HealthBarComponent.rb
+# Enrique Rodriguez
+# Created 2014/12/05
+# Copyright © 2014 DigiPen Institute of Technology, All Rights Reserved
+#######################
+
+class HealthBarComponent < ComponentBase
+  def initialize(data)
+    super data
+    
+    register_event :health_changed, :on_change
+    register_event :update, :first_update
+  end
+  
+  def first_update(e)
+    remove_event :update
+    @max_health = owner.parent.defense_component.health
+  end
+
+  def on_change(e)
+    health = owner.parent.defense_component.health
+
+    bar = self.owner.transform_component
+    bar.scale = Vector.new *[health / @max_health, 0.5, 1]
+  end
+
+  register_component "HealthBarComponent"
+end

abaa95bcb37328e87079034a06f0b9c215c8bae7
Connor Hilarides <connorcpu@live.com>
2015-03-17 18:25:52 -0700

    All fixed

--- EntityEditor/Entities/Representations/EntityView.xaml.cs
+++ EntityEditor/Entities/Representations/EntityView.xaml.cs
@@ -2,6 +2,7 @@
 using System.Windows;
 using System.Windows.Controls;
 using EntityEditor.Entities.Project;
+using EntityEditor.Helpers;
 
 namespace EntityEditor.Entities.Representations
 {
@@ -37,7 +38,30 @@ namespace EntityEditor.Entities.Representations
 
         private void AddComponentClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            var editor = this.GetParent<Editor>();
+            editor.ComponentSelector.Select();
+
+            editor.ComponentFlyout.IsOpenChanged += ComponentFlyoutOnClose;
+        }
+
+        private void ComponentFlyoutOnClose(object sender, RoutedEventArgs routedEventArgs)
+        {
+            var editor = this.GetParent<Editor>();
+            if (editor.ComponentFlyout.IsOpen)
+                return;
+
+            editor.ComponentFlyout.IsOpenChanged -= ComponentFlyoutOnClose;
+
+            var component = editor.ComponentSelector.SelectedComponent;
+            var entity = (Entity) DataContext;
+
+            if (component == null)
+                return;
+
+            if (!entity.NewComponent(component.Name))
+            {
+                MessageBox.Show("Component already exists");
+            }
         }
 
         private void RemoveComponent(object sender, RoutedEventArgs e)

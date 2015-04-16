099bdb88ed510c204e66158be0eb357537c9d4c5
Connor Hilarides <connorcpu@live.com>
2015-04-15 20:20:47 -0700

    Hell yeh

--- Roguelike/Assets/Scripts/Components/Misc/GameDisplays.rb
+++ Roguelike/Assets/Scripts/Components/Misc/GameDisplays.rb
@@ -11,7 +11,7 @@ def init_floor_text(entity)
   if !GAME_STATE[:tutorial].nil?
     owner.raise_event :send, [:set_text_at, [0, ""]]
   else
-    score = "Score #{GAME_STATE[:score]}"
+    score = "Scrap #{GAME_STATE[:score]}"
 
     maxfloor = GAME_STATE[:endless] ? "" : " / #{$DungeonLength}"
     floor = "Floor #{GAME_STATE[:floor]}#{maxfloor}\nGet to the stairs!"
@@ -19,4 +19,17 @@ def init_floor_text(entity)
     message = "#{score}\n#{floor}"
     owner.raise_event :send, [:set_text_at, [0, message]]
   end
+end
+
+def display_final_score!
+  texts = find_entity("Score!").text_component.texts
+
+  GAME_STATE[:score] += 50 # Completion bonus
+
+  scoremsg = "Your final scrap collection is #{GAME_STATE[:score]} units"
+  profitmsg = "Company profit is $#{GAME_STATE[:score] - 20}"
+  lolgg = "Your share is $20 and a free oil change"
+  texts[0] = "#{scoremsg}\n#{profitmsg}\n#{lolgg}"
+
+  GAME_STATE[:score] = 0
 end
